dee76eed65db88fff4d76593d05d930bf5125118
Антон Кондратов <hobocta@gmail.com>
2018-12-06 00:16:24 +0300

    Рефакторинг

--- src/Encryptor/Fabric/OpenSslEncryptorFabric.php
+++ src/Encryptor/Fabric/OpenSslEncryptorFabric.php
@@ -11,7 +11,7 @@ class OpenSslEncryptorFabric extends AbstractEncryptorFabric
         return new OpenSslEncryptor(
             $this->key,
             array(
-                'method' => 'AES-128-CBC', // see more methods: openssl_get_cipher_methods()
+                'method' => 'aes-128-cbc', // see more methods: openssl_get_cipher_methods()
                 'options' => OPENSSL_RAW_DATA, // disable base64 encode in openssl_encrypt to get raw data
             )
         );
@@ -22,7 +22,7 @@ class OpenSslEncryptorFabric extends AbstractEncryptorFabric
         return new OpenSslEncryptor(
             $this->key,
             array(
-                'method' => 'AES-192-CBC', // see more methods: openssl_get_cipher_methods()
+                'method' => 'aes-192-cbc', // see more methods: openssl_get_cipher_methods()
                 'options' => OPENSSL_RAW_DATA, // disable base64 encode in openssl_encrypt to get raw data
             )
         );
@@ -33,7 +33,7 @@ class OpenSslEncryptorFabric extends AbstractEncryptorFabric
         return new OpenSslEncryptor(
             $this->key,
             array(
-                'method' => 'AES-256-CBC', // see more methods: openssl_get_cipher_methods()
+                'method' => 'aes-256-cbc', // see more methods: openssl_get_cipher_methods()
                 'options' => OPENSSL_RAW_DATA, // disable base64 encode in openssl_encrypt to get raw data
             )
         );
