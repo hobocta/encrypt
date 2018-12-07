0e98ca022afbcb984ba5282f1b561b9c614af136
Антон Кондратов <hobocta@gmail.com>
2018-12-07 16:25:17 +0300

    Сделал возможность проверить и OpenSSL и Mcrypt одновременно

--- examples/all.php
+++ examples/all.php
@@ -1,7 +1,11 @@
 <?php
 
-use Hobocta\Encrypt\Encryptor\Fabric\EncryptorFabric;
+use Hobocta\Encrypt\Encryptor\Implementation\Mcrypt\McryptAvailableChecker;
+use Hobocta\Encrypt\Encryptor\Implementation\Mcrypt\McryptEncryptorFabric;
+use Hobocta\Encrypt\Encryptor\Implementation\OpenSsl\OpenSslAvailableChecker;
+use Hobocta\Encrypt\Encryptor\Implementation\OpenSsl\OpenSslEncryptorFabric;
 use Hobocta\Encrypt\EncryptService;
+use Hobocta\Encrypt\Exception\EncryptException;
 use Hobocta\Encrypt\Stringify\Base64Stringify;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -9,33 +13,46 @@ require __DIR__ . '/../vendor/autoload.php';
 $password = '1234';
 $data = 'My secret data!';
 
+$encryptorFabrics = array();
+
 try {
     $key = hash('sha1', $password);
 
-    $encryptorFabric = new EncryptorFabric($key);
-} catch (Exception $e) {
+    if (OpenSslAvailableChecker::isAvailable()) {
+        $encryptorFabrics['OpenSSL'] = new OpenSslEncryptorFabric($key);
+    }
+
+    if (McryptAvailableChecker::isAvailable()) {
+        $encryptorFabrics['Mcrypt'] =  new McryptEncryptorFabric($key);
+    }
+} catch (EncryptException $e) {
     die(sprintf('Exception message: %s (%s:%s)', $e->getMessage(), $e->getFile(), $e->getLine()) . PHP_EOL);
 }
 
-$encryptors = array(
-    '128 bit' => $encryptorFabric->createEncryptor128(),
-    '192 bit' => $encryptorFabric->createEncryptor192(),
-    '256 bit' => $encryptorFabric->createEncryptor256(),
-);
-
-foreach ($encryptors as $encryptorName => $encryptor) {
+foreach ($encryptorFabrics as $name => $encryptorFabric) {
     echo PHP_EOL;
-    echo $encryptorName . ':' . PHP_EOL;
+    echo $name . ':' . PHP_EOL;
 
-    $encryptService = new EncryptService($encryptor, new Base64Stringify());
+    $encryptors = array(
+        '128 bit' => $encryptorFabric->createEncryptor128(),
+        '192 bit' => $encryptorFabric->createEncryptor192(),
+        '256 bit' => $encryptorFabric->createEncryptor256(),
+    );
 
-    echo 'Data: ' . var_export($data, true) . PHP_EOL;
+    foreach ($encryptors as $encryptorName => $encryptor) {
+        echo PHP_EOL;
+        echo $encryptorName . ':' . PHP_EOL;
 
-    $encrypted = $encryptService->encrypt($data);
-    echo 'Encrypted: ' . var_export($encrypted, true) . PHP_EOL;
+        $encryptService = new EncryptService($encryptor, new Base64Stringify());
 
-    $decrypted = $encryptService->decrypt($encrypted);
-    echo 'Decrypted: ' . var_export($decrypted, true) . PHP_EOL;
+        echo 'Data: ' . var_export($data, true) . PHP_EOL;
 
-    echo 'Result: ' . ($data === $decrypted ? 'Equals' : 'Failure') . PHP_EOL;
+        $encrypted = $encryptService->encrypt($data);
+        echo 'Encrypted: ' . var_export($encrypted, true) . PHP_EOL;
+
+        $decrypted = $encryptService->decrypt($encrypted);
+        echo 'Decrypted: ' . var_export($decrypted, true) . PHP_EOL;
+
+        echo 'Result: ' . ($data === $decrypted ? 'Equals' : 'Failure') . PHP_EOL;
+    }
 }
--- examples/performance.php
+++ examples/performance.php
@@ -1,7 +1,12 @@
 <?php
 
 use Hobocta\Encrypt\Encryptor\Fabric\EncryptorFabric;
+use Hobocta\Encrypt\Encryptor\Implementation\Mcrypt\McryptAvailableChecker;
+use Hobocta\Encrypt\Encryptor\Implementation\Mcrypt\McryptEncryptorFabric;
+use Hobocta\Encrypt\Encryptor\Implementation\OpenSsl\OpenSslAvailableChecker;
+use Hobocta\Encrypt\Encryptor\Implementation\OpenSsl\OpenSslEncryptorFabric;
 use Hobocta\Encrypt\EncryptService;
+use Hobocta\Encrypt\Exception\EncryptException;
 use Hobocta\Encrypt\Stringify\Base64Stringify;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -9,37 +14,49 @@ require __DIR__ . '/../vendor/autoload.php';
 $password = '1234';
 $data = 'My secret data!';
 
+$encryptorFabrics = array();
+
 try {
     $key = hash('sha1', $password);
 
-    $encryptorFabric = new EncryptorFabric($key);
-} catch (Exception $e) {
+    if (OpenSslAvailableChecker::isAvailable()) {
+        $encryptorFabrics['OpenSSL'] = new OpenSslEncryptorFabric($key);
+    }
+
+    if (McryptAvailableChecker::isAvailable()) {
+        $encryptorFabrics['Mcrypt'] =  new McryptEncryptorFabric($key);
+    }
+} catch (EncryptException $e) {
     die(sprintf('Exception message: %s (%s:%s)', $e->getMessage(), $e->getFile(), $e->getLine()) . PHP_EOL);
 }
 
-$encryptors = array(
-    '128 bit' => $encryptorFabric->createEncryptor128(),
-    '192 bit' => $encryptorFabric->createEncryptor192(),
-    '256 bit' => $encryptorFabric->createEncryptor256(),
-);
+foreach ($encryptorFabrics as $name => $encryptorFabric) {
+    echo $name . ':' . PHP_EOL;
 
-$count = 10000;
+    $encryptors = array(
+        '128 bit' => $encryptorFabric->createEncryptor128(),
+        '192 bit' => $encryptorFabric->createEncryptor192(),
+        '256 bit' => $encryptorFabric->createEncryptor256(),
+    );
 
-foreach ($encryptors as $encryptorName => $encryptor) {
-    $encryptService = new EncryptService($encryptor, new Base64Stringify());
+    $count = 10000;
 
-    echo $encryptorName . ': ';
+    foreach ($encryptors as $encryptorName => $encryptor) {
+        $encryptService = new EncryptService($encryptor, new Base64Stringify());
 
-    $time = microtime(true);
+        echo $encryptorName . ': ';
 
-    for ($i = 0; $i < $count; $i++) {
-        $dataTmp = $data . uniqid();
-        $encrypted = $encryptService->encrypt($dataTmp);
-        $decrypted = $encryptService->decrypt($encrypted);
-        if ($dataTmp !== $decrypted) {
-            die('Failure');
+        $time = microtime(true);
+
+        for ($i = 0; $i < $count; $i++) {
+            $dataTmp = $data . uniqid();
+            $encrypted = $encryptService->encrypt($dataTmp);
+            $decrypted = $encryptService->decrypt($encrypted);
+            if ($dataTmp !== $decrypted) {
+                die('Failure');
+            }
         }
-    }
 
-    echo '' . $count . ' encryptions and decryptions at ' . (microtime(true) - $time) . ' s' . PHP_EOL;
+        echo '' . $count . ' encryptions and decryptions at ' . (microtime(true) - $time) . ' s' . PHP_EOL;
+    }
 }
