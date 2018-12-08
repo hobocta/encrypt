c633045c0d6c9db301e648962bbfbc86eeb3ad27
Антон Кондратов <hobocta@gmail.com>
2018-12-08 14:06:09 +0300

    Перенёс скрипты ручных тестов в отдельную папку

--- tests/manual/coverage.sh
+++ tests/manual/coverage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 /f/OSPanel/modules/php/PHP-5.6-x64/php.exe ~/bin/phpunit-5.7.27.phar --coverage-html=tests/coverage
 /f/OSPanel/modules/php/PHP-5.6-x64/php.exe ~/bin/phpunit-5.7.27.phar --coverage-clover=tests/coverage/clover.xml
-source coverage_env.sh
+source tests/manual/env.sh
 bash <(curl -s https://codecov.io/bash)
