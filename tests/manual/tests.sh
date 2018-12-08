3c621e47c7d137978fefa4e865581cde77bd5984
Антон Кондратов <hobocta@gmail.com>
2018-12-08 18:19:17 +0300

    phpunit

--- tests/manual/tests.sh
+++ tests/manual/tests.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-clear && \
-echo "remove phpunit/phpunit" && \
-/f/OSPanel/modules/php/PHP-7.2-x64/php.exe ~/bin/composer.phar remove phpunit/phpunit --no-interaction --dev -q && \
-echo "require phpunit/phpunit" && \
-/f/OSPanel/modules/php/PHP-5.3/php.exe ~/bin/composer.phar require phpunit/phpunit:^4 --no-interaction --dev -q && \
+#clear && \
+#echo "remove phpunit/phpunit" && \
+#/f/OSPanel/modules/php/PHP-7.2-x64/php.exe ~/bin/composer.phar remove phpunit/phpunit --no-interaction --dev -q && \
+#echo "require phpunit/phpunit" && \
+#/f/OSPanel/modules/php/PHP-5.3/php.exe ~/bin/composer.phar require phpunit/phpunit:^4 --no-interaction --dev -q && \
 /f/OSPanel/modules/php/PHP-5.3/php.exe ~/bin/phpunit-4.8.36.phar -v && \
 /f/OSPanel/modules/php/PHP-5.4/php.exe ~/bin/phpunit-4.8.36.phar -v && \
 /f/OSPanel/modules/php/PHP-5.5/php.exe ~/bin/phpunit-4.8.36.phar -v && \
 /f/OSPanel/modules/php/PHP-5.5-x64/php.exe ~/bin/phpunit-4.8.36.phar -v && \
-echo "remove phpunit/phpunit" && \
-/f/OSPanel/modules/php/PHP-7.2-x64/php.exe ~/bin/composer.phar remove phpunit/phpunit --no-interaction --dev -q && \
-echo "require phpunit/phpunit" && \
-/f/OSPanel/modules/php/PHP-7.2-x64/php.exe ~/bin/composer.phar require "phpunit/phpunit:^5.7" --no-interaction --prefer-source --dev -q && \
+#echo "remove phpunit/phpunit" && \
+#/f/OSPanel/modules/php/PHP-7.2-x64/php.exe ~/bin/composer.phar remove phpunit/phpunit --no-interaction --dev -q && \
+#echo "require phpunit/phpunit" && \
+#/f/OSPanel/modules/php/PHP-7.2-x64/php.exe ~/bin/composer.phar require "phpunit/phpunit:^5.7" --no-interaction --prefer-source --dev -q && \
 /f/OSPanel/modules/php/PHP-5.6/php.exe ~/bin/phpunit-5.7.27.phar -v && \
 /f/OSPanel/modules/php/PHP-5.6-x64/php.exe ~/bin/phpunit-5.7.27.phar -v && \
 /f/OSPanel/modules/php/PHP-7.0/php.exe ~/bin/phpunit-6.5.13.phar -v && \
