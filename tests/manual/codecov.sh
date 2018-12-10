ffda44bfe20c0d7bee46c97b4bb6cf687a1507d0
Антон Кондратов <hobocta@gmail.com>
2018-12-10 12:48:10 +0300

    Разделил ручной запуск coverage и отправку в codecov

---
+++ tests/manual/codecov.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+source tests/manual/env.sh
+bash <(curl -s https://codecov.io/bash)
--- tests/manual/coverage.sh
+++ tests/manual/coverage.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-/f/OSPanel/modules/php/PHP-5.6-x64/php.exe ~/bin/phpunit-5.7.27.phar --coverage-html=tests/coverage
-/f/OSPanel/modules/php/PHP-5.6-x64/php.exe ~/bin/phpunit-5.7.27.phar --coverage-clover=tests/coverage/clover.xml
-source tests/manual/env.sh
-bash <(curl -s https://codecov.io/bash)
+/f/OSPanel/modules/php/PHP-5.6-x64/php.exe ~/bin/phpunit-5.7.27.phar --coverage-html=tests/coverage --coverage-clover=tests/coverage/clover.xml
